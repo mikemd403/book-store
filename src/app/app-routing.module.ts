f2994df354ae60e4f689ddefe279e4e82d987215
Mike D <mdiep044@uottawa.ca>
2020-09-30 17:09:32 -0400

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { BookComponent } from './books/book/book.component';
+import { BooksComponent } from './books/books.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const booksRoutes: Routes = [
+  {path:':id',component:BookComponent}
+];
+
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'about',component:AboutComponent},
+  {path:'contact',component:ContactComponent},
+  {path:'books',component:BooksComponent,
+    children:booksRoutes},
+  {path:'',redirectTo:'home',pathMatch:'full'},
+  {path:'**',component:HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
